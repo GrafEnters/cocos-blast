648a79f36528b2fdd20c27d2a426d241333150d9
Graf Enters <graf.enters@gmail.com>
2026-02-11 18:00:21 +0400

    bomb chain works

--- assets/Script/GameCore/BombBoosterExtension.ts
+++ assets/Script/GameCore/BombBoosterExtension.ts
@@ -23,6 +23,19 @@ export default class BombBoosterExtension implements IBoosterExtension {
         }
 
         const removed: { row: number; col: number }[] = [];
+        const used: { [key: string]: boolean } = {};
+
+        const pushRemoved = (r: number, c: number) => {
+            const key = r + "_" + c;
+            if (used[key]) {
+                return;
+            }
+            used[key] = true;
+            removed.push({ row: r, col: c });
+        };
+
+        const scoreBefore = model.getScore();
+        let directRemovedCount = 0;
 
         for (let r = row - this.radius; r <= row + this.radius; r++) {
             for (let c = col - this.radius; c <= col + this.radius; c++) {
@@ -33,8 +46,23 @@ export default class BombBoosterExtension implements IBoosterExtension {
                 if (value === null) {
                     continue;
                 }
-                removed.push({ row: r, col: c });
+                if (model.isSuperTile(r, c)) {
+                    const id = model.getSuperTileId(r, c);
+                    if (!id) {
+                        continue;
+                    }
+                    const chainResult = model.handleSuperTileChain(id, r, c);
+                    if (chainResult && chainResult.removed && chainResult.removed.length > 0) {
+                        for (let i = 0; i < chainResult.removed.length; i++) {
+                            const cell = chainResult.removed[i];
+                            pushRemoved(cell.row, cell.col);
+                        }
+                    }
+                    continue;
+                }
                 model.setCellValue(r, c, null);
+                pushRemoved(r, c);
+                directRemovedCount++;
             }
         }
 
@@ -42,8 +70,12 @@ export default class BombBoosterExtension implements IBoosterExtension {
             return null;
         }
 
-        const scoreDelta = model.calculateGroupScorePublic(removed.length);
-        model.applyScorePublic(scoreDelta);
+        if (directRemovedCount > 0) {
+            const scoreDeltaDirect = model.calculateGroupScorePublic(directRemovedCount);
+            model.applyScorePublic(scoreDeltaDirect);
+        }
+
+        const scoreDelta = model.getScore() - scoreBefore;
 
         return {
             removed,
