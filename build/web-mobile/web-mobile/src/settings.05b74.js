13691722fe72a976aaf6f32e17aea0acfdd3897e
Graf Enters <graf.enters@gmail.com>
2026-02-13 14:53:37 +0400

    added build

---
+++ build/web-mobile/web-mobile/src/settings.05b74.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/CoreScene.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d17e2",resources:"90265",main:"e5443"}};
